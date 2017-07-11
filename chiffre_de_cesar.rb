b6b74f0da6b007eb3b87fc3301edccf36d7c6f0c
ydede404 <dakitse@gmail.com>
2017-07-11 16:32:37 +0200

    Update chiffre_de_cesar.rb
    Correction de Yasmine

--- chiffre_de_cesar.rb
+++ chiffre_de_cesar.rb
@@ -2,10 +2,12 @@ def chiffre_de_cesar(mot, chiffre)
   result = ""
   m = mot.length - 1
   for c in (0..m)
-    ascii = mot[c].ord + num
+    ascii = mot[c].ord + chiffre    #c'est quoi num? la variable n'est pas défini, il faut remplacer par chiffre
     result += ascii.chr
   end
-    puts result
-  end
+  puts result
+end
 
 chiffre_de_cesar("Hello", 3)
+
+#englobal: il manque le rebouclage entre z et a
