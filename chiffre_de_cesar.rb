ac700c2cefd2a5b226d13a5ac3f678f3fff666c3
ydede404 <dakitse@gmail.com>
2017-07-11 16:38:24 +0200

    Update chiffre_de_cesar.rb

--- chiffre_de_cesar.rb
+++ chiffre_de_cesar.rb
@@ -2,10 +2,13 @@ def chiffre_de_cesar(mot, chiffre)
   result = ""
   m = mot.length - 1
   for c in (0..m)
-    ascii = mot[c].ord + num
+    ascii = mot[c].ord + num #la variable num n'existe pas, utilisé chiffre
     result += ascii.chr
   end
     puts result
   end
 
 chiffre_de_cesar("Hello", 3)
+#le programme marche si tu remplace num par chiffre, par contre, z ne reboucle pas avec a...
+
+
